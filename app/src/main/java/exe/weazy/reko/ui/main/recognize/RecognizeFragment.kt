77ece2f754f450fa568be4e7d37468f9122eb618
weazyexe <weazy.exe@gmail.com>
2020-05-09 13:04:34 +0300

    3-camera | make recognize screen

--- app/src/main/java/exe/weazy/reko/ui/main/recognize/RecognizeFragment.kt
+++ app/src/main/java/exe/weazy/reko/ui/main/recognize/RecognizeFragment.kt
@@ -4,12 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import exe.weazy.reko.R
+import exe.weazy.reko.util.handleTopInsets
+import kotlinx.android.synthetic.main.fragment_recognize.*
 
 class RecognizeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_recognize, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        handleTopInsets(profileToolbarLayout)
+        initListeners()
+    }
+
+    private fun initListeners() {
+        photoButton.setOnClickListener {
+            Toast.makeText(requireContext(), "Photo", Toast.LENGTH_SHORT).show()
+        }
+
+        chooseButton.setOnClickListener {
+            Toast.makeText(requireContext(), "Choose", Toast.LENGTH_SHORT).show()
+        }
     }
 }
