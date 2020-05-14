14fed47067f1848c14b858eae75f755714577fd4
weazyexe <weazy.exe@gmail.com>
2020-05-14 20:37:36 +0300

    5-api-recognize | add photo from gallery recognize

--- app/src/main/java/exe/weazy/reko/util/utils.kt
+++ app/src/main/java/exe/weazy/reko/util/utils.kt
@@ -1,7 +1,5 @@
 package exe.weazy.reko.util
 
-import android.content.Context
-import android.net.Uri
 import android.os.Build
 import android.view.View
 import androidx.core.view.ViewCompat
@@ -9,7 +7,6 @@ import androidx.core.view.marginBottom
 import androidx.core.view.updatePadding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import exe.weazy.reko.util.extensions.updateMargin
-import java.io.File
 import kotlin.random.Random
 
 
@@ -50,10 +47,4 @@ fun generateId(length: Int = 10): String {
         sb.append(symbols[Random.nextInt(symbols.length)])
     }
     return sb.toString()
-}
-
-fun Uri.toFileUri(): Uri {
-    val file = File(path) //create path from uri
-    val splitted = file.path.split(':')
-    return Uri.fromFile(File(splitted[1]))
 }
